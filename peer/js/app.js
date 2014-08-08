3963c4db4e37ffa483fe3455e28735d9fa3600fb
Gursimran Singh <g@kilotau.com>
2014-08-08 16:41:06 -0500

    Display list of connected peers on every peer

--- peer/js/app.js
+++ peer/js/app.js
@@ -1,17 +1,44 @@
-'use strict';
+/** @jsx React.DOM */
+$(function() {
 
-var peer = new Peer(chance.guid(), {host: 'localhost', port: 9001, path: '/peer_server'});
+  var peer = new Peer(chance.guid(), {host: 'localhost', port: 9001, path: '/peer_server'});
 
-peer.on('open', function(id) {
-  console.log('I am ' + id);
+  peer.on('open', function(id) {
+    console.log('I am ' + id);
+  });
+
+  // Define what peers are and how to display them...
+
+  var peers = [];
+
+  var getPeers = function() {
+    $.getJSON('connected', function(data){
+      var mapped = _.map(data, function(e) {return {id: e, name: "Peer "+e}});
+      peers = _.union(peers, mapped);
+      // Render with updated information
+      React.renderComponent(<PeerList peers={peers}/>, $('#react-wrapper').get(0));
+    });
+  };
+
+  /* React Components */
+
+  var PeerList  = React.createClass({
+    render: function() {
+      var peers = this.props.peers;
+      return (
+      <ul>
+        {peers.map(function(peer) {
+          return <li key={peer.id}>{peer.name}</li>;
+        })}
+      </ul>
+      );
+    }
+  });
+
+  // Load and refresh peer list every 2 seconds...
+
+  getPeers();
+  window.setInterval(function(){
+    getPeers();
+  }, 2000);
 });
-
-var Peers = [];
-
-var getPeers = function() {
-  $.getJSON('connected', function(data){
-    return Peers = _.union(Peers, data);
-  })
-};
-
-getPeers();
