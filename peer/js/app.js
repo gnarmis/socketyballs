68d8c3aaeca870e7dcfda364c62568cf117eabeb
Gursimran Singh <g@kilotau.com>
2014-08-08 20:19:39 -0500

    Balls balls balls

--- peer/js/app.js
+++ peer/js/app.js
@@ -1,7 +1,9 @@
 /** @jsx React.DOM */
 $(function() {
 
-  var peer = new Peer(chance.guid(), {host: 'localhost', port: 9001, path: '/peer_server'});
+  var my_id = chance.guid();
+
+  var peer = new Peer(my_id, {host: 'localhost', port: 9001, path: '/peer_server'});
 
   peer.on('open', function(id) {
     console.log('I am ' + id);
@@ -10,13 +12,25 @@ $(function() {
   // Define what peers are and how to display them...
 
   var peers = [];
+  window.inConns = [];
+  window.outConns = [];
+
+  peer.on("connection", function(conn) {
+    console.log("Received a connection from " + conn.peer);
+    window.inConns = _.union(window.inConns, [connection]);
+  });
 
   var getPeers = function() {
     $.getJSON('connected', function(data){
       var mapped = _.map(data, function(e) {return {id: e, name: "Peer "+e}});
       peers = mapped;
       // Render with updated information
-      React.renderComponent(<PeerList peers={peers}/>, $('#react-wrapper').get(0));
+      //React.renderComponent(<PeerList peers={peers}/>, $('#react-wrapper').get(0));
+      // Connect to all of them
+      window.outConns = _.map(_.filter(peers, function(x) {return x.id!=my_id}), function(e) {
+        console.log("Connecting to " + e.id);
+        peer.connect(e.id);
+      });
     });
   };
 
@@ -35,10 +49,15 @@ $(function() {
     }
   });
 
-  // Load and refresh peer list every 2 seconds...
+  // Ball Handling...
 
+  window.numBalls=1;
+
+  // Load and re-render every 2 seconds...
   getPeers();
   window.setInterval(function(){
     getPeers();
-  }, 2000);
+    window.numBalls = 1+_.size(_.uniq(_.union(window.inConns, window.outConns)));
+    console.log(window.numBalls);
+  }, 500);
 });
